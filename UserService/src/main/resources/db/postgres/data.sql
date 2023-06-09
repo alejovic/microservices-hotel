4a41d7b08110793200b1129b86a71b4ebaaf91d1
avictoria <alejovic@gmail.com>
2023-06-09 14:26:59 +1000

    Include liquibase

---
+++ UserService/src/main/resources/db/postgres/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.micro_users
+(id, about, email, "name")
+VALUES(1, 'Software Engineer', 'alejandro@gmail.com', 'Alejandro');
+
+INSERT INTO public.micro_users
+(id, about, email, "name")
+VALUES(2, 'Software Tester', 'daniel@gmail.com', 'Daniel');
+
+INSERT INTO public.micro_users
+(id, about, email, "name")
+VALUES(3, 'Software Tester II', 'aura@gmail.com', 'aura');
