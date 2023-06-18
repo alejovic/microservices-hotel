df0b2a334ba46c961ca393b39c880f4c491d9d44
avictoria <alejovic@gmail.com>
2023-06-18 14:16:50 +1000

    Rating Microservice App - Mongo DB + Liquibase (Mongo)

---
+++ RatingService/src/main/java/com/avg/LiquibaseConfiguration.java
@@ -0,0 +1,27 @@
+package com.avg;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableConfigurationProperties({LiquibaseProperties.class})
+public class LiquibaseConfiguration {
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource mongoDB, LiquibaseProperties liquibaseProperties) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(mongoDB);
+        liquibase.setChangeLog(liquibaseProperties.getChangeLog());
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setShouldRun(liquibaseProperties.isEnabled());
+        return liquibase;
+    }
+
+}
