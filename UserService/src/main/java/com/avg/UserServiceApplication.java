7c776bba05492b1ae298f389c1e35e7f04c0db65
avictoria <alejovic@gmail.com>
2023-07-01 22:50:52 +1000

    Eureka Service Registry Client implementation

--- UserService/src/main/java/com/avg/UserServiceApplication.java
+++ UserService/src/main/java/com/avg/UserServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableEurekaClient is deprecated , no need to annotate the main class. It is enough to add the spring-cloud-starter-netflix-eureka-client
 public class UserServiceApplication {
 
     public static void main(String[] args) {
